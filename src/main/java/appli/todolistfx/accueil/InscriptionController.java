474c108d86f6254ea514763bc159989109baec4a
Baldium <alexandre.marescaux@outlook.fr>
2024-09-24 11:24:10 +0200

    rajout de inscription controller et inscription view

---
+++ src/main/java/appli/todolistfx/accueil/InscriptionController.java
@@ -0,0 +1,86 @@
+package appli.todolistfx.accueil;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InscriptionController {
+
+    @FXML
+    private PasswordField confirmerMdp;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private Button inscriptionButton;
+
+    @FXML
+    private Label inscriptionText;
+
+    @FXML
+    private Label inscriptionText1;
+
+    @FXML
+    private Label inscriptionText2;
+
+    @FXML
+    private Label inscriptionText3;
+
+    @FXML
+    private Label inscriptionText4;
+
+    @FXML
+    private Label inscriptionText5;
+
+    @FXML
+    private PasswordField motDePasse;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prenom;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    void afficherConfirmerMdp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void afficherMail(ActionEvent event) {
+
+    }
+
+    @FXML
+    void afficherMdp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void afficherNom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void afficherPrenom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inscription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mdpOublie(ActionEvent event) {
+
+    }
+
+}
